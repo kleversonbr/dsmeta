f28ffaac93af99e79b45f8511c7e72cc5fec87a6
Kleverson Brandalize <kleversonbr@gmail.com>
2022-09-07 17:20:22 -0300

    Project created

---
+++ backend/src/main/java/com/dsmeta/sdsmeta/SdsMetaApplication.java
@@ -0,0 +1,13 @@
+package com.dsmeta.sdsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsMetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dsmeta/sdsmeta/SdsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmeta.sdsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
